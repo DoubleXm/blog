a787f7f132010e6b1a9730a61dd8fc5160237a64
xsq <xu18516321273@qq.com>
2023-08-25 17:43:03 +0800

    docs: py

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -259,6 +259,7 @@ function createPySidebar() {
         { text: '数据类型、运算符', link: '/docs/py/basic/数据类型-运算符' },
         { text: '数据类型（高级）', link: '/docs/py/basic/数据类型-高级' },
         { text: '分支、循环、函数、文件IO、异常捕获、模块', link: '/docs/py/basic/循环-函数' },
+        { text: '面向对象', link: '/docs/py/basic/面向对象' },
       ]
     }
   ];
