b78471ef5c3a36bb8e05cda11d00a2dd3852ccfe
xsq <xu18516321273@qq.com>
2025-01-08 16:07:51 +0800

    feat: ImagePreview 组件& vue-router-4 流程图

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,9 +1,45 @@
-import NextImage, { ImageProps } from 'next/image'
+'use client';
 
+import { useLayoutEffect, useState } from 'react';
+import NextImage, { ImageProps } from 'next/image'
+import ImagePreview from './ImagePreview'
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => {
+  const baseSrc = `${basePath || ''}${src}`;
+  const [isPreview, setIsPreview] = useState(false);
+
+  function scrollHandler(e: Event) {
+    e.preventDefault();
+  }
+
+  useLayoutEffect(() => {
+    if (isPreview) {
+      window.addEventListener('wheel', scrollHandler, { passive: false });
+    }
+    return () => {
+      window.removeEventListener('wheel', scrollHandler);
+    }
+  }, [isPreview]);
+
+  return <div className='relative'>
+    <NextImage
+      style={{ cursor: 'zoom-in' }}
+      src={baseSrc}
+      onClick={() => {
+        setIsPreview(true);
+      }}
+      {...rest}
+    />
+    <ImagePreview
+      style={{ opacity: isPreview ? 1 : 0, zIndex: isPreview ? 100 : -1 }} 
+      src={baseSrc} 
+      onClose={() => {
+        setIsPreview(false);
+        window.removeEventListener('wheel', scrollHandler);
+      }} 
+    />
+  </div>
+};
 
 export default Image
---
+++ components/ImagePreview.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+interface ImagePreviewProps {
+  src: string;
+  onClose: () => void;
+  style?: React.CSSProperties;
+}
+
+const ImagePreview = ({ src, onClose, style }: ImagePreviewProps) => {
+  return <div 
+    className="preview-wrapper" 
+    onClick={
+      (e) => {
+        e.stopPropagation();
+        onClose();
+      }
+    }
+    style={style}
+  >
+    <div className="preview-outer">
+      <img className="perview-img" src={src} />
+    </div>
+  </div>
+}
+
+export default ImagePreview
