8ab09717e4b060702d63e217e7a0570dd6de3824
xsq <xu18516321273@qq.com>
2025-01-08 16:17:40 +0800

    style: lint code

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,45 +1,47 @@
-'use client';
+'use client'
 
-import { useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react'
 import NextImage, { ImageProps } from 'next/image'
 import ImagePreview from './ImagePreview'
 const basePath = process.env.BASE_PATH
 
 const Image = ({ src, ...rest }: ImageProps) => {
-  const baseSrc = `${basePath || ''}${src}`;
-  const [isPreview, setIsPreview] = useState(false);
+  const baseSrc = `${basePath || ''}${src}`
+  const [isPreview, setIsPreview] = useState(false)
 
   function scrollHandler(e: Event) {
-    e.preventDefault();
+    e.preventDefault()
   }
 
   useLayoutEffect(() => {
     if (isPreview) {
-      window.addEventListener('wheel', scrollHandler, { passive: false });
+      window.addEventListener('wheel', scrollHandler, { passive: false })
     }
     return () => {
-      window.removeEventListener('wheel', scrollHandler);
+      window.removeEventListener('wheel', scrollHandler)
     }
-  }, [isPreview]);
+  }, [isPreview])
 
-  return <div className='relative'>
-    <NextImage
-      style={{ cursor: 'zoom-in' }}
-      src={baseSrc}
-      onClick={() => {
-        setIsPreview(true);
-      }}
-      {...rest}
-    />
-    <ImagePreview
-      style={{ opacity: isPreview ? 1 : 0, zIndex: isPreview ? 100 : -1 }} 
-      src={baseSrc} 
-      onClose={() => {
-        setIsPreview(false);
-        window.removeEventListener('wheel', scrollHandler);
-      }} 
-    />
-  </div>
-};
+  return (
+    <div className="relative">
+      <NextImage
+        style={{ cursor: 'zoom-in' }}
+        src={baseSrc}
+        onClick={() => {
+          setIsPreview(true)
+        }}
+        {...rest}
+      />
+      <ImagePreview
+        style={{ opacity: isPreview ? 1 : 0, zIndex: isPreview ? 100 : -1 }}
+        src={baseSrc}
+        onClose={() => {
+          setIsPreview(false)
+          window.removeEventListener('wheel', scrollHandler)
+        }}
+      />
+    </div>
+  )
+}
 
 export default Image
--- components/ImagePreview.tsx
+++ components/ImagePreview.tsx
@@ -1,26 +1,26 @@
-'use client';
+'use client'
 
 interface ImagePreviewProps {
-  src: string;
-  onClose: () => void;
-  style?: React.CSSProperties;
+  src: string
+  onClose: () => void
+  style?: React.CSSProperties
 }
 
 const ImagePreview = ({ src, onClose, style }: ImagePreviewProps) => {
-  return <div 
-    className="preview-wrapper" 
-    onClick={
-      (e) => {
-        e.stopPropagation();
-        onClose();
-      }
-    }
-    style={style}
-  >
-    <div className="preview-outer">
-      <img className="perview-img" src={src} />
+  return (
+    <div
+      className="preview-wrapper"
+      onClick={(e) => {
+        e.stopPropagation()
+        onClose()
+      }}
+      style={style}
+    >
+      <div className="preview-outer">
+        <img className="perview-img" src={src} />
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default ImagePreview
